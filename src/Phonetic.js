ddaac2097773e830775a0ae941cbc46b0bb9aa51
AshJungers <97846504+AshJungers@users.noreply.github.com>
2022-05-24 17:39:07 -0600

    updated component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Phonetics(props) {
+export default function Phonetic(props) {
   console.log(props.phonetic);
   return "Hello from phonics";
 }
