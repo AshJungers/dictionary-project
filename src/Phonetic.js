fff0cba686a32ac967787249461588084460112d
AshJungers <97846504+AshJungers@users.noreply.github.com>
2022-05-29 14:50:44 -0600

    Created new component
    Created phonics component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,5 +2,13 @@ import React from "react";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
-  return "Hello from phonics";
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank">
+        Listen
+      </a>
+      <br />
+      {props.phonetic.text}
+    </div>
+  );
 }
