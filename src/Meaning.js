258c019ac3a375a5b741be734561d3dc132e189a
AshJungers <97846504+AshJungers@users.noreply.github.com>
2022-05-31 10:30:06 -0600

    Fixing meaning css

--- src/Meaning.js
+++ src/Meaning.js
@@ -10,13 +10,11 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <div className="definition">{definition.definition}</div>
-
             <div className="example">{definition.example}</div>
-
-            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
